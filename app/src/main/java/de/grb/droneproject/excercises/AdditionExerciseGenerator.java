073ef04187ea3daec08bb066b29e4c3e5dfd1cc6
mark <>
2023-01-28 19:38:20 +0100

    add negative numbers to subtraction and clamp z values.

--- app/src/main/java/de/grb/droneproject/excercises/AdditionExerciseGenerator.java
+++ app/src/main/java/de/grb/droneproject/excercises/AdditionExerciseGenerator.java
@@ -1,13 +1,29 @@
 package de.grb.droneproject.excercises;
 
+import java.util.Random;
+
 import de.grb.droneproject.vectormath.Vector3D;
 
 public class AdditionExerciseGenerator extends AddSubExercise {
 
     @Override
     public void Generate() {
-        firstVector = new Vector3D(10, 1);
-        secondVector = new Vector3D(10, 1);
+        firstVector = new Vector3D(10, 0);
+        secondVector = new Vector3D(10, 0);
+        Random random = new Random();
+
+        // result vectors z HAS TO BE between 0 and 2 (prevent flying into ground or too high)
+        int z3 = random.nextInt(3);
+
+        // first vector has z between 0 and z3
+        int z1 = z3 > 0 ? random.nextInt(z3) : 0;
+
+        // choose second vectors z to accommodate for result vector with z between 0 and 2
+        int z2 = z3 - z1;
+
+        firstVector.setZ(z1);
+        secondVector.setZ(z2);
+
         solution = firstVector.add(secondVector);
     }
 
--- app/src/main/java/de/grb/droneproject/excercises/SubtractionExerciseGenerator.java
+++ app/src/main/java/de/grb/droneproject/excercises/SubtractionExerciseGenerator.java
@@ -1,13 +1,29 @@
 package de.grb.droneproject.excercises;
 
+import java.util.Random;
+
 import de.grb.droneproject.vectormath.Vector3D;
 
 public class SubtractionExerciseGenerator extends AddSubExercise {
 
     @Override
     public void Generate() {
-        firstVector = new Vector3D(10, 0);
-        secondVector = new Vector3D(10, 0);
+        firstVector = new Vector3D(10, -9);
+        secondVector = new Vector3D(10, -9);
+        Random random = new Random();
+
+        // result vectors z HAS TO BE between 0 and 2 (prevent flying into ground or too high)
+        int z3 = random.nextInt(3);
+
+        // first vector has z between -9 and 9
+        int z1 = random.nextInt(10 - (-9)) - 9;
+
+        // choose second vectors z to accommodate for result vector with z between 0 and 2
+        int z2 = z1 - z3;
+
+        firstVector.setZ(z1);
+        secondVector.setZ(z2);
+
         solution = firstVector.sub(secondVector);
     }
 
