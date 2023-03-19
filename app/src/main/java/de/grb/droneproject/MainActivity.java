244b48499befb10cc175b59999fa5fdb3980c7af
LeSchlongLong <46922337+LeSchlongLong@users.noreply.github.com>
2023-03-19 23:33:19 +0100

    javadoc

--- app/src/main/java/de/grb/droneproject/MainActivity.java
+++ app/src/main/java/de/grb/droneproject/MainActivity.java
@@ -59,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
         The following navigation bar currently not in use, as at the time of the initial release,
         there is only addition exercises.
 
+        @author K.Donato
+
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_nav);
         bottomNavigationView.setSelectedItemId(R.id.plus);
 
