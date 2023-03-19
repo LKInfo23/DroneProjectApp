a6620693b8bd36bbe8fffb20b4791ffa44082432
mark <>
2023-03-19 09:41:39 +0100

    constraint fixes to exercise generation + javadoc.

--- app/src/main/java/de/grb/droneproject/excercises/AdditionExerciseGenerator.java
+++ app/src/main/java/de/grb/droneproject/excercises/AdditionExerciseGenerator.java
@@ -10,25 +10,55 @@ import de.grb.droneproject.vectormath.Vector3D;
 public class AdditionExerciseGenerator extends AddSubExercise {
 
     /**
-     * Generates the vectors for the addition exercise. First it generates two random vectors and then clamps the z value
-     * before using the resulting vectors to generate the solution vector
+     * Generates the vectors for the addition exercise.
+     *
+     * @apiNote
+     * Vectors are chosen to accommodate for physical limitations of space.
+     * We only have around 5*5*2 meters that work well for our purposes, which is
+     * why the resulting vectors are guaranteed to have:
+     * X- and Y-components of between -5 and 5 inclusive.
+     * Z-components of between -5 and 5 inclusive.
+     *
+     * @author L. Janke
+     * @author M. Hagen
      */
     @Override
     public void Generate() {
-        firstVector = new Vector3D(6, -5);
-        secondVector = new Vector3D(6, -5);
+        firstVector = new Vector3D();
+        secondVector = new Vector3D();
+
         Random random = new Random();
 
         // result vectors z HAS TO BE between 0 and 2 (prevent flying into ground or too high)
         int z3 = random.nextInt(3);
 
         // first vector has z between 0 and z3
-        int z1 = z3 > 0 ? random.nextInt(z3) : 0;
+        int z1 = z3 > 0 ? random.nextInt(z3 + 1) : 0;
 
         // choose second vectors z to accommodate for result vector with z between 0 and 2
         int z2 = z3 - z1;
 
+
+        // result vectors x and y HAS TO BE between -5 and 5
+        int x3 = random.nextInt(11) - 5;
+        int y3 = random.nextInt(11) - 5;
+
+        // first vector has x and y between -9 and 9 respectively
+        int x1 = random.nextInt(19) - 9;
+        int y1 = random.nextInt(19) - 9;
+
+        // choose second vectors x and y to accommodate for result vector with x3 and y3
+        int x2 = x3 - x1;
+        int y2 = y3 - y1;
+
+
+        // now the vectors are populated with the components that meet our constraints
+        firstVector.setX(x1);
+        firstVector.setY(y1);
         firstVector.setZ(z1);
+
+        secondVector.setX(x2);
+        secondVector.setY(y2);
         secondVector.setZ(z2);
 
         solution = firstVector.add(secondVector);
--- app/src/main/java/de/grb/droneproject/excercises/SubtractionExerciseGenerator.java
+++ app/src/main/java/de/grb/droneproject/excercises/SubtractionExerciseGenerator.java
@@ -7,22 +7,56 @@ import de.grb.droneproject.vectormath.Vector3D;
 
 public class SubtractionExerciseGenerator extends AddSubExercise {
 
+    /**
+     * Generates the vectors for the subtraction exercise.
+     *
+     * @apiNote
+     * Vectors are chosen to accommodate for physical limitations of space.
+     * We only have around 5*5*2 meters that work well for our purposes, which is
+     * why the resulting vectors are guaranteed to have:
+     * X- and Y-components of between -5 and 5 inclusive.
+     * Z-components of between -5 and 5 inclusive.
+     *
+     * @author L. Janke
+     * @author M. Hagen
+     */
     @Override
     public void Generate() {
-        firstVector = new Vector3D(10, -9);
-        secondVector = new Vector3D(10, -9);
+        firstVector = new Vector3D();
+        secondVector = new Vector3D();
+
         Random random = new Random();
 
         // result vectors z HAS TO BE between 0 and 2 (prevent flying into ground or too high)
         int z3 = random.nextInt(3);
 
-        // first vector has z between -9 and 9
-        int z1 = random.nextInt(10 - (-9)) - 9;
+        // first vector has z between 0 and z3
+        int z1 = z3 > 0 ? random.nextInt(z3 + 1) : 0;
 
         // choose second vectors z to accommodate for result vector with z between 0 and 2
-        int z2 = z1 - z3;
+        int z2 = z3 - z1;
 
+
+        // result vectors x and y HAS TO BE between -5 and 5
+        int x3 = random.nextInt(11) - 5;
+        int y3 = random.nextInt(11) - 5;
+
+        // first vector has x and y between -9 and 9 respectively
+        int x1 = random.nextInt(19) - 9;
+        int y1 = random.nextInt(19) - 9;
+
+        // choose second vectors x and y to accommodate for result vector with x3 and y3
+        int x2 = x1 - x3;
+        int y2 = y1 - y3;
+
+
+        // now the vectors are populated with the components that meet our constraints
+        firstVector.setX(x1);
+        firstVector.setY(y1);
         firstVector.setZ(z1);
+
+        secondVector.setX(x2);
+        secondVector.setY(y2);
         secondVector.setZ(z2);
 
         solution = firstVector.sub(secondVector);
